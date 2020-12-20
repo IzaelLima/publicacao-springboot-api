10e315a34f2c94258d4471140f5719679dbb6542
Izael <izael_lima@hotmail.com>
2020-12-20 14:55:39 -0300

    ajuste

--- src/main/resources/migration/CREATE_TABLE_PUBLICACAO.sql
+++ src/main/resources/migration/CREATE_TABLE_PUBLICACAO.sql
@@ -1,13 +1,6 @@
-CREATE TABLE public.publicacao
-(
-    id bigint NOT NULL DEFAULT nextval('publicacao_id_seq'::regclass),
-    data_publicacao timestamp without time zone,
-    texto character varying(255) COLLATE pg_catalog."default",
-    total_likes integer,
-    CONSTRAINT publicacao_pkey PRIMARY KEY (id)
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE public.publicacao
-    OWNER to postgres;
+CREATE TABLE publicacao (
+	id serial PRIMARY KEY,
+	data_publicacao timestamp without time zone,
+	texto character varying(255),
+	total_likes integer
+);
